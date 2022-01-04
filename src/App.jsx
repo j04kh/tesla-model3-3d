174fa3fe11c0b114a4a8f457e11be832d5930d55
Joaquin <joaquinhernandezph@gmail.com>
2022-01-04 11:56:18 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div className="h-screen w-screen bg-black">
-      <h1 className="text-3xl text-white">Hello</h1>
-    </div>
-  );
+  return <div className="h-screen w-screen bg-black"></div>;
 }
 
 export default App;
